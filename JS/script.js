b1f040b00fec500fad4adef2cab334260213fba4
TheresiaAurelly <theresiaAurelly@gmail.com>
2023-09-07 15:03:46 +0700

    Update loginRegister, home, product

--- JS/script.js
+++ JS/script.js
@@ -15,3 +15,25 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 3000); // Change after 3 second
 }
+
+function plusSlides(n) {
+    showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+    var slides = document.getElementsByClassName("slide");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
